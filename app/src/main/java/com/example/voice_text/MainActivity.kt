1d445535c3f853262f2fc98806fe87f0e863b6d8
badri_akkala <lcb2021029@iiitl.ac.in>
2022-11-10 12:56:47 +0530

    step 2 : speech input function

--- app/src/main/java/com/example/voice_text/MainActivity.kt
+++ app/src/main/java/com/example/voice_text/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.voice_text
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.widget.Toast
+import com.example.voice_text.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    //request code constant
+    private val RQ_SPEECH_REC = 69
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //
+        binding.btnButton.setOnClickListener {
+            askSpeechInput()
+        }
+    }
+
+    //
+    private fun askSpeechInput(){
+        if(!SpeechRecognizer.isRecognitionAvailable(this)){
+            Toast.makeText(this,"speech recognition is not available",Toast.LENGTH_SHORT).show()
+        }
+        else{
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())   // select any language here(here we used mobile default language)
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Listening...")
+            startActivityForResult(intent,RQ_SPEECH_REC)
+
+        }
     }
 }
